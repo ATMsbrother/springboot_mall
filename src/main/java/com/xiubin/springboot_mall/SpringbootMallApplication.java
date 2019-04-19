b3752ece0c52bc93515b85ca529a61a1b1257fe5
ATMsbrother <cxbzs@foxmail.com>
2019-04-19 11:22:47 +0800

    使用idea创建的一个最简单的springboot项目

---
+++ src/main/java/com/xiubin/springboot_mall/SpringbootMallApplication.java
@@ -0,0 +1,15 @@
+package com.xiubin.springboot_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class})
+public class SpringbootMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMallApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/xiubin/springboot_mall/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.xiubin.springboot_mall.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @FileName
+ * @Author cxb
+ * @Date 2019/4/19 11:17
+ * @Description
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
